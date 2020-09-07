06d7d45cfb8e270cd26e472f6405be5212e0276f
Venkatsainathreddy <64485287+Venkatsainathreddy@users.noreply.github.com>
2020-09-07 10:12:44 +0530

    Add files via upload

---
+++ lib/Research.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Research extends StatelessWidget{
+   Widget build(BuildContext context){
+     return Container(
+       height: 100,
+       width: 100
+     );
+   }
+}
